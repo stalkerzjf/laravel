847374b1116679434f3c8510e5aae55813f6bc48
stalkerzjf <stalkerzjf@163.com>
2019-05-31 16:08:25 +0800

    测试格式化

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
-        return view('home');
+        echo 'I`m Ok';
+                $this->validate($request, [
+                    'pid' => 'required',
+                    'uid' => 'int',
+                ]);
+        return $request->all();
+//        return view('home');
     }
 
     public function help()
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 
-Route::get('/', 'StaticPagesController@home');
+Route::any('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
 
